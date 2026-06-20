57211e84cf30bcc5f43cc427d174803b8bde19a3
Mark Ninnim <markninnim@Mac-mini.local>
2026-06-20 20:38:50 +0100

    Move It's a fact text to top of page

--- server.js
+++ server.js
@@ -236,7 +236,7 @@ app.get('/personalise-brochure', requireAuth, async (req, res) => {
       if (factp) {
         const { height: fph } = factp.getSize();
         const factSize = 36;
-        const fy = fph - 620; // "It’s a fact..." is low on the page
+        const fy = fph - 110; // "It’s a fact..." near top of page
         const fx = 55;
         factp.drawRectangle({ x: fx - 2, y: fy - 6, width: 420, height: factSize + 14, color: bgColour });
         factp.drawText(`${firstName}, it’s a fact...`, { x: fx, y: fy, size: factSize, font, color: orange });
